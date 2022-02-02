2d732fb637aecf78a4248d97a5c2a351c894bca5
Jonathan Trang <jttrang96@gmail.com>
2022-02-01 18:27:38 -0800

    Completed javascript-functions-practice exercise.

---
+++ javascript-functions-practice/get-last-character.js
@@ -0,0 +1,6 @@
+/* exported getLastCharacter */
+function getLastCharacter(string) {
+  var lastStringIndex = string.length - 1;
+  var lastCharacter = string.charAt(lastStringIndex);
+  return lastCharacter;
+}
