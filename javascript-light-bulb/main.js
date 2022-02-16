a0e7d2b40f4da9cfb7feb18b22b7c1a1d2bac2f8
Jonathan Trang <jttrang96@gmail.com>
2022-02-16 13:25:51 -0800

    Revised to include a conditional.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,15 @@
 var $circle = document.querySelector('.circle');
 var $container = document.querySelector('.container');
-console.log($container.className);
+var isOn = true;
 
 function changeColor(event) {
-  if ($circle.className === 'circle light-mode-circle light-mode-box-shadow') {
+  isOn = !isOn;
+  if (isOn) {
+    $circle.className = ('circle light-mode-circle light-mode-box-shadow');
+    $container.className = ('container light-mode-background');
+  } else {
     $container.className = ('container dark-mode-background');
     $circle.className = ('circle dark-mode-circle dark-mode-box-shadow');
-  } else { $circle.className = ('circle light-mode-circle light-mode-box-shadow');
-    $container.className = ('container light-mode-background');
   }
 }
 
