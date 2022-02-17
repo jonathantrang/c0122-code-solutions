b2ca4429554e9ecdbc00052bd4a850f74ba1cd20
Jonathan Trang <jttrang96@gmail.com>
2022-02-17 15:40:08 -0800

    Revised javascript-view-swapping exercise. Condensed code.

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -4,15 +4,13 @@ var $viewList = document.querySelectorAll('.view');
 
 function tabSwitch(event) {
   var currentTab = event.target;
+  var dataView = event.target.getAttribute('data-view');
   if (currentTab.matches('.tab')) {
     for (var i = 0; i < $tabList.length; i++) {
       if ($tabList[i] === currentTab) {
         $tabList[i].className = 'tab active';
       } else $tabList[i].className = 'tab';
     }
-  }
-  if (currentTab) {
-    var dataView = event.target.getAttribute('data-view');
     for (var i = 0; i < $viewList.length; i++) {
       if ($viewList[i].getAttribute('data-view') === dataView) {
         $viewList[i].className = 'view';
