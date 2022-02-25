2cf863397e72538c0b2bbf2041a472fe2856398d
Jonathan Trang <jttrang96@gmail.com>
2022-02-25 00:22:08 -0800

    Completed javascript-carousel challenge.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,7 +1,9 @@
 var $container = document.querySelector('.container');
 var $cheveronList = document.querySelectorAll('.cheveron');
 var $carouselList = document.querySelectorAll('.carousel');
+var carouselLength = $carouselList.length;
 var $imageList = document.querySelectorAll('.pokemon');
+var index = 0;
 
 function carouselChange(event) {
   var currentCarousel = event.target;
@@ -10,41 +12,58 @@ function carouselChange(event) {
   if (currentCarousel.matches('.carousel')) {
     for (var i = 0; i < $carouselList.length; i++) {
       if ($carouselList[i] === currentCarousel) {
+        index = i;
         $carouselList[i].className = 'carousel fas fa-circle';
       } else $carouselList[i].className = 'carousel far fa-circle';
     }
     for (i = 0; i < $imageList.length; i++) {
       if ($imageList[i].getAttribute('data-view') === pokemonView) {
+        index = i;
         $imageList[i].className = 'pokemon';
       } else $imageList[i].className = 'pokemon hidden';
     }
   }
 }
 
+function cheveronRightInterval() {
+    $carouselList[index].className = 'carousel far fa-circle';
+    $imageList[index].className = 'pokemon hidden';
+    if (index === carouselLength - 1) {
+      index = 0;
+      $carouselList[index].className = 'carousel fas fa-circle';
+      $imageList[index].className = 'pokemon';
+    } else {
+      index++;
+      $carouselList[index].className = 'carousel fas fa-circle';
+      $imageList[index].className = 'pokemon';
+    }
+}
+
 function cheveronChange(event) {
   var cheveronCheck = event.target;
-  var currentCarousel = event.target;
-
-  var pokemonView = event.target.getAttribute('data-view');
-
   if (cheveronCheck === $cheveronList[0]) {
-    for (var i = $carouselList.length - 1; i >= 0; i--) {
-      var carouselView.-[i] ss
-      if (carouselView[i] === currentCarousel) {
-        $carouselList[i].className = 'carousel fas fa-circle';
-      } else $carouselList[i].className = 'carousel far fa-circle';
+    $carouselList[index].className = 'carousel far fa-circle';
+    $imageList[index].className = 'pokemon hidden'
+    if (index === 0) {
+      index = carouselLength - 1;
+      $carouselList[index].className = 'carousel fas fa-circle';
+      $imageList[index].className = 'pokemon';
+    } else {
+        index--;
+        $carouselList[index].className = 'carousel fas fa-circle';
+        $imageList[index].className = 'pokemon';
     }
-  // } else if (cheveronCheck === $cheveronList[1] && currentCarousel.matches('carousel')) {
-  //   for (i = 0; i < $carouselList.length; i++) {
-  //     if ($carouselList[i] === currentCarousel) {
-  //       $carouselList[i].className = 'carousel fas fa-circle';
-  //     } else $carouselList[i].className = 'carousel far fa-circle';
-  //   }
-  // }
+  }
+  if (cheveronCheck === $cheveronList[1]) {
+    cheveronRightInterval();
   }
 }
 
+function pokemonInterval() {
+  cheveronRightInterval();
+}
+
+
 $container.addEventListener('click', carouselChange);
 $container.addEventListener('click', cheveronChange);
-
-// add a function that goes back one carousel or forward one carousel depending on which cheveron is clicked
+var intervalID = setInterval(pokemonInterval, 3000);
