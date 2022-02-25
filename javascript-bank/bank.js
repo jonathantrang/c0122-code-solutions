3b26bb0a45a3ead6550b5498923a0b9cbdddc966
Jonathan Trang <jttrang96@gmail.com>
2022-02-25 20:28:36 +0000

    minor changes.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,20 +1,28 @@
 /* exported Bank */
 Bank.prototype.openAccount = function (holder, balance) {
-  if (balance > 0) {
-    var account = new Account(nexAccountNumber, holder, balance);
+  if (balance < 0) {
+    return null;
+  } else if (balance > 0) {
+    var account = new Account(this.nextAccountNumber, holder);
     this.accounts.push(account);
-  } else return null;
-}
+    this.nextAccountNumber++;
+    return this.nextAccountNumber - 1;
+  }
+};
 Bank.prototype.getAccount = function (number) {
-
-}
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (number === this.accounts[i]) {
+      return Account();
+    }
+  }
+};
 Bank.prototype.getTotalAssets = function () {
   if (this.accounts.length === 0) {
     return 0;
   } else {
     return (this.accounts.length - 1);
   }
-}
+};
 
 function Bank() {
   var nextAccountNumber = 1;
