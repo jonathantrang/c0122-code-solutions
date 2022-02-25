509e576ee4ce221bdac9d3da6bb9c8a9a2365acd
Jonathan Trang <jttrang96@gmail.com>
2022-02-25 09:35:02 -0800

    Finished account portion but stuck on bank.

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,7 +1,31 @@
 /* exported Account */
-Transaction.prototype.deposit = function () {};
-Transaction.prototype.withdraw = function () {};
-Transaction.prototype.getBalance = function () {};
+Account.prototype.deposit = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var transaction = new Transaction('deposit', amount);
+    this.transactions.push(transaction);
+    return true;
+  } else return false;
+};
+Account.prototype.withdraw = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var withdrawal = new Transaction('withdrawal', amount);
+    this.transactions.push(withdrawal);
+    return true;
+  } else return false;
+};
+Account.prototype.getBalance = function () {
+  if (this.transactions.length === 0) {
+    return 0;
+  } else {
+    var balance = 0;
+    for (var i = 0; i < this.transactions.length; i++) {
+      if (this.transactions[i].type === 'withdrawal') {
+        balance -= this.transactions[i].amount;
+      } else balance += this.transactions[i].amount;
+    }
+    return balance;
+  }
+};
 
 function Account(number, holder) {
   this.number = number;
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,7 +1,24 @@
 /* exported Bank */
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance > 0) {
+    var account = new Account(nexAccountNumber, holder, balance);
+    this.accounts.push(account);
+  } else return null;
+}
+Bank.prototype.getAccount = function (number) {
+
+}
+Bank.prototype.getTotalAssets = function () {
+  if (this.accounts.length === 0) {
+    return 0;
+  } else {
+    return (this.accounts.length - 1);
+  }
+}
+
 function Bank() {
-  var accountNumber = 1;
-  this.acccountNumber = accountNumber;
+  var nextAccountNumber = 1;
+  this.nextAccountNumber = nextAccountNumber;
   var accounts = [];
   this.accounts = accounts;
 }
