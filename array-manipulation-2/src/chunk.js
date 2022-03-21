03dd0ec2cbb2039a5c0f9b5b45051f80af284abe
Jonathan Trang <jonathantrang.js@gmail.com>
2022-03-20 22:23:56 -0700

    Completed array-manipulation-2 algos.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -7,25 +7,17 @@
 2. Clarify.
   If array is [1, 2, 3, 4] and the size is 2 then returns [[1, 2], [3, 4].
     How to get?
-      Create a variable that stores the main array
-        Push sub arrays into main array
-      Create a variable that stores the sub arrays
-        These sub arrays have items pushed into it dependent on lenth of array divided by size
-      Create a variable that represents the new array count
-  If the count is -1 then it returns nothing
-  If the array is empty? Returns an empty array
+      Create a variable that stores the new array
 
 3. Pseudocode.
   Define a function with the parameters of array and size.
-  Create a condition statement that if size is less than 0 then returns the whole array
-  Create a condition statement that if array is empty then return an empty array
-  Create a condition statement that if size is more than 0 then code will run
   Create a variable that is an empty array
   Create a loop
     Initializes at 0
     Condition for the loop to go is if the initialization is less than the array.length
-      If it is then push the value of the array at an index of array into the empty array
-    Increment by 1
+      If it is then push a new array into the empty array storage
+        The new array will start at the incrementing value and end at the incrementing value + whatever the size parameter is
+    Increment by the incrementing value plus the given size parameter
   Return the empty array.
 
 4. Trace.
@@ -34,21 +26,9 @@
 */
 
 function chunk(array, size) {
-  const count = array.length % size;
   const newArray = [];
-  if (count === 0) {
-    for (let i = 0; i < array.length / size; i++) {
-      newArray.push([]);
-      for (let j = 0; j < size; j++) {
-        newArray[i].push(array[j]);
-      }
+  for (let i = 0; i < array.length; i+=size) {
+    newArray.push(array.slice(i, i + size));
     }
-    return newArray;
-  }
-
-  // else if (count > 0) {
-
-  // }
-  // const count
-  // else if
+  return newArray;
 }
