a5831a800027cbe3b0d211b9b30f5f7c2fc5f7b9
Jonathan Trang <jonathantrang.js@gmail.com>
2022-03-22 15:11:19 -0700

    Currently on omit part of algos.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,16 @@
 /* exported omit */
+/*
+1. Understand
+  Given two parameters (source an object, keys an array of strings)
+  Return a new object containing the properties of source that is not listed in keys
+2. Clarify
+  Removes object properties that match with the given key
+  If keys don't match with object then return the whole object
+  If object is empty then return an empty object
+3. Pseudocode
+  Define a function with parameters source and keys
+
+4. Trace
+5. Code
+6. Trace
+*/
