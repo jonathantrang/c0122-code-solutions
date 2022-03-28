2609076725eca0162ca9c4e90fbda836765b23cc
Jonathan Trang <jonathantrang.js@gmail.com>
2022-03-28 18:10:19 +0000

    Completed es6-classes exercise.

---
+++ es6-classes/student.js
@@ -0,0 +1,24 @@
+/* exported Student */
+
+class Student {
+  constructor(firstName, lastName, subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+  }
+
+  getStudent() {
+    return this.student;
+  }
+}
+
+Student.prototype.getFullName = function () {
+  const { firstName, lastName } = this;
+  return `${firstName} ${lastName}`;
+};
+
+Student.prototype.getIntroduction = function () {
+  const { subject } = this;
+  const fullName = this.getFullName();
+  return `Hello, my name is ${fullName} and I am studying ${subject}.`;
+};
