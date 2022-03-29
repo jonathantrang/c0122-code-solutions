2538425c7d901f649c13b3e0aef748e054ea3608
Jonathan Trang <jonathantrang.js@gmail.com>
2022-03-29 19:35:19 +0000

    Stuck on conditional statement part of omit problem.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -9,8 +9,25 @@
   If object is empty then return an empty object
 3. Pseudocode
   Define a function with parameters source and keys
+  Have a variable that stores the new object containing source parameter not listed in keys parameter
+  Create a loop
+    Set an initial variable at 0
+    Conditional set where if initial variable is less than the string length then runs conditional code block
+      Create an if statmement that checks to see if
+    Increment the initial variable by 1
+  Checks to see if
 
 4. Trace
 5. Code
 6. Trace
 */
+
+const omit = (source, keys) => {
+  const newObj = {};
+  for (let i = 0; i < keys.length; i++) {
+    if (source[keys[i]] === keys[i] in source) {
+      newObj[keys[i]] = source[keys[i]];
+    }
+  }
+  return newObj;
+}
