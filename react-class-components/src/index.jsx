c973a9ee349e5b1d7423c79d786e7ba50f4d474a
Jonathan Trang <jonathantrang.js@gmail.com>
2022-03-30 17:34:49 +0000

    Completed react-class-components exercise.

---
+++ react-class-components/src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  render() {
+    return <button>{this.props.text}</button>;
+  }
+}
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
